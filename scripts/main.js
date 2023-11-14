7f43342540a8f22aea0196784f7fd757e891696d
kukla1989 <kukla.1989@ukr.net>
2023-11-14 16:13:49 +0200

    add data on page from object, add button show more/show less

---
+++ scripts/main.js
@@ -0,0 +1,94 @@
+const book = {
+  name: "The Da Vinci Code",
+  'cover': "https://m.media-amazon.com/images/I/81jtrIKJd2L._AC_UF1000,1000_QL80_.jpg",
+  author: "Dan Brown",
+  rating: 4,
+  'page-count': 534,
+  language: "English",
+  genre: "Mystery",
+  date: "March 31, 2009",
+  description: "#1 WORLDWIDE BESTSELLER • While in Paris, Harvard symbologist Robert Langdon is awakened by a phone call in the dead of the night. The elderly curator of the Louvre has been murdered inside the museum, his body covered in baffling symbols.\n" +
+    "\n" +
+    "        “Blockbuster perfection.... A gleefully erudite suspense novel.” —The New York Times\n" +
+    "\n" +
+    "        “A pulse-quickening, brain-teasing adventure.” —People\n" +
+    "\n" +
+    "        As Langdon and gifted French cryptologist Sophie Neveu sort through the bizarre riddles, they are stunned to discover a trail of clues hidden in the works of Leonardo da Vinci—clues visible for all to see and yet ingeniously disguised by the painter.\n" +
+    "\n" +
+    "        Even more startling, the late curator was involved in the Priory of Sion—a secret society whose members included Sir Isaac Newton, Victor Hugo, and Da Vinci—and he guarded a breathtaking historical secret. Unless Langdon and Neveu can decipher the labyrinthine puzzle—while avoiding the faceless adversary who shadows their every move—the explosive, ancient truth will be lost forever."
+}
+
+validateBook(book)
+addBookDataOnPage(book)
+addFunctionToMoreButton()
+
+function addBookDataOnPage(book) {
+  for (const key in book) {
+    const element = document.querySelector(`.book__${key}`);
+
+    if (!element) {
+      continue;
+    }
+
+    if (key === 'cover') {
+      element.src = book[key];
+      element.alt = book.name;
+
+      continue;
+    }
+
+    if (key === 'rating') {
+      element.classList.add(`stars--${book[key]}`)
+
+      continue
+    }
+
+    element.innerHTML = book[key]
+  }
+}
+
+function validateBook(book) {
+  const requiredKeys = ['name', 'cover', 'author', 'rating', 'page-count', 'language', 'genre', 'date', 'description']
+  const bookKeys = Object.keys(book)
+
+  requiredKeys.forEach(requiredKey => {
+    if (!bookKeys.includes(requiredKey)) {
+      throw new Error(`book should have property "${requiredKey}"`)
+    }
+  })
+
+  const requiredRating = Array.from({ length: 5 }, (_, ind) => ind + 1)
+  if (!requiredRating.includes(Number(book.rating))) {
+    throw new Error(`book.rating is "${book.rating}" but should be 1 || 2 || 3 || 4 || 5`)
+  }
+
+  const requiredStringKeys = requiredKeys
+    .filter(key => key !== 'rating' && key !== 'page-count' && key !== 'name')
+  requiredStringKeys.forEach(stringKey => {
+    if (typeof book[stringKey] !== 'string') {
+      throw new Error(`book.${stringKey} is "${book[stringKey]}" but its should be string`)
+    }
+  })
+}
+
+function addFunctionToMoreButton() {
+  const descriptionText = document.querySelector('.book__description');
+  const moreButton = document.querySelector('.book__more');
+
+  if (descriptionText.innerHTML.length > 400) {
+    moreButton.style.display = 'inline-block';
+  }
+
+  moreButton.onclick = function () {
+    console.log('ass')
+    if(descriptionText.classList.contains("book__description--open")){
+      //shrink the box
+      descriptionText.classList.remove("book__description--open")
+      moreButton.innerHTML = "SHOW MORE";
+    } else {
+      //expand the box
+      descriptionText.classList.add("book__description--open")
+      moreButton.innerHTML = "SHOW LESS";
+    }
+  };
+}
